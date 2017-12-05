22faecfe4740cd239036e734444742f284f02bd2
Juan David Martinez Rios <jdmartinez859@misena.edu.co>
2017-12-04 22:33:20 -0500

    se agregan cambios para que funciones el backend

--- src/main/java/co/edu/sena/pruebaJava/jpa/entities/Parqueadero.java
+++ src/main/java/co/edu/sena/pruebaJava/jpa/entities/Parqueadero.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,7 +6,6 @@
 package co.edu.sena.pruebajava.jpa.entities;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,8 +14,6 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
